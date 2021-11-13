2f5a1bc84b98eb8f0adcf913e7d42f3a5f773737
GoodDonkey <president20500@gmail.com>
2021-11-13 23:09:41 +0900

    reference type variables: Date

--- src/com/codewithme/Main.java
+++ src/com/codewithme/Main.java
@@ -1,13 +1,12 @@
 package com.codewithme;
 
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
         byte age = 30;
-        long viewsCount = 3_123_456_789L;
-        float price = 10.99F;
-        char letter = 'A';
-        boolean isEligible = true;
-
+        Date now = new Date();  // instance of Date class
+        System.out.println(now);
     }
 }
