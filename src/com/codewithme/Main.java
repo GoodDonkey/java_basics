cf99c87de5105e0c71c7b1c6963ef9f7a82e21b7
hungrydonkey <president20500@gmail.com>
2021-11-14 08:27:02 +0900

    Arithmetic Expressions

--- src/com/codewithme/Main.java
+++ src/com/codewithme/Main.java
@@ -3,9 +3,28 @@ package com.codewithme;
 public class Main {
 
     public static void main(String[] args) {
-        // constant
-        final float pi = 3.14F;
-        pi = 1;
-        System.out.println(pi);
+//        int result = 10 + 3;
+//        double result = (double)10 / (double)3;
+//        System.out.println(result);
+
+//        int x = 1;
+//        x++; // 1을 더함
+//        System.out.println(x);
+
+//        int x = 1;
+//        int y = x++;
+//        System.out.println(x);
+//        System.out.println(y);
+
+//        int x = 1;
+//        int y = ++x;  // +1을 한 후 y에 할당
+//        System.out.println(x);
+//        System.out.println(y);
+
+        // augmented, compound assignment operator
+        int x = 1;
+        x += 2;
+        System.out.println(x);
+
     }
 }
