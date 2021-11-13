6d46be2f0d3bd5839c3b2534caa435fccb05dd21
hungrydonkey <president20500@gmail.com>
2021-11-14 08:35:24 +0900

    Casting: Explicit Casting: casting string to double

--- src/com/codewithme/Main.java
+++ src/com/codewithme/Main.java
@@ -3,10 +3,10 @@ package com.codewithme;
 public class Main {
 
     public static void main(String[] args) {
-        // explicit casting (automatic casting)
+        // explicit casting
         // byte > short > int > long > float > double
-        String x = "1"; // String이리면
-        int y = Integer.parseInt(x) + 2; // 이렇게 int로 casting해줘야 한다.
+        String x = "1.1";
+        Double y = Double.parseDouble(x) + 2;
         System.out.println(y);
     }
 }
