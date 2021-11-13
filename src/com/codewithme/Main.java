6e63c486d133936e57da9b840a705982b9ad9caf
GoodDonkey <president20500@gmail.com>
2021-11-14 00:49:24 +0900

    Array: multidimensional array, deepToString

--- src/com/codewithme/Main.java
+++ src/com/codewithme/Main.java
@@ -6,13 +6,11 @@ import java.util.Locale;
 public class Main {
 
     public static void main(String[] args) {
-        // array: int array인 number를 길이 5로 정의
-        int[] numbers = { 2, 3, 5, 1, 4};
-        System.out.println(numbers.length); // fixed length
-        System.out.println(Arrays.toString(numbers)); // int[]는 int로 initialize
-
-        Arrays.sort(numbers); // 정렬하기
-        System.out.println(Arrays.toString(numbers)); // int[]는 int로 initialize
+        // multidimensional array
+        int[][] numbers = new int[2][3];
+        numbers[0][0] = 1;
+        System.out.println(Arrays.toString(numbers)); // 주소값만 준다.
+        System.out.println(Arrays.deepToString(numbers));
 
     }
 }
