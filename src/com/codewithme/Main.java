2be8f7d362fcacae78e1744f6b5688833ea5962c
hungrydonkey <president20500@gmail.com>
2021-11-14 08:35:24 +0900

    Casting: Explicit Casting: casting string to int

--- src/com/codewithme/Main.java
+++ src/com/codewithme/Main.java
@@ -5,9 +5,8 @@ public class Main {
     public static void main(String[] args) {
         // explicit casting (automatic casting)
         // byte > short > int > long > float > double
-        double x = 1.1;
-        int y = (int)x + 2;
+        String x = "1"; // String이리면
+        int y = Integer.parseInt(x) + 2; // 이렇게 int로 casting해줘야 한다.
         System.out.println(y);
-
     }
 }
