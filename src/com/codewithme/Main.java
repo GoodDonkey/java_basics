a634915d67e481ec2c620644b87ff6b4bfd60732
GoodDonkey <president20500@gmail.com>
2021-11-14 01:17:14 +0900

    constant: 'final' keyword make the variable not to chnage

--- src/com/codewithme/Main.java
+++ src/com/codewithme/Main.java
@@ -1,13 +1,11 @@
 package com.codewithme;
 
-import java.util.Arrays;
-import java.util.Locale;
-
 public class Main {
 
     public static void main(String[] args) {
-        // multidimensional array
-        int[][] numbers = { {1, 2, 3}, {4, 5, 6}};
-        System.out.println(Arrays.deepToString(numbers));
+        // constant
+        final float pi = 3.14F;
+        pi = 1;
+        System.out.println(pi);
     }
 }
