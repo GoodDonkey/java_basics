489083f0173785f5337e6012f57aaebf1c60defb
hungrydonkey <president20500@gmail.com>
2021-11-14 08:35:24 +0900

    Casting: Explicit Casting

--- src/com/codewithme/Main.java
+++ src/com/codewithme/Main.java
@@ -3,9 +3,10 @@ package com.codewithme;
 public class Main {
 
     public static void main(String[] args) {
-        // Implicit casting (automatic casting)
-        short x = 1; // 2byte
-        int y = x + 2; //
+        // explicit casting (automatic casting)
+        // byte > short > int > long > float > double
+        double x = 1.1;
+        int y = (int)x + 2;
         System.out.println(y);
 
     }
