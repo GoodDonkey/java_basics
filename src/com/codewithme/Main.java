d561f99348e9687095dafcad589bdcb992a3aeaa
hungrydonkey <president20500@gmail.com>
2021-11-14 08:27:02 +0900

    Order of Operation

--- src/com/codewithme/Main.java
+++ src/com/codewithme/Main.java
@@ -3,27 +3,8 @@ package com.codewithme;
 public class Main {
 
     public static void main(String[] args) {
-//        int result = 10 + 3;
-//        double result = (double)10 / (double)3;
-//        System.out.println(result);
-
-//        int x = 1;
-//        x++; // 1을 더함
-//        System.out.println(x);
-
-//        int x = 1;
-//        int y = x++;
-//        System.out.println(x);
-//        System.out.println(y);
-
-//        int x = 1;
-//        int y = ++x;  // +1을 한 후 y에 할당
-//        System.out.println(x);
-//        System.out.println(y);
-
-        // augmented, compound assignment operator
-        int x = 1;
-        x += 2;
+//        int x = 10 + 3 * 2;
+        int x = (10 + 3) * 2; // 괄호가 먼저
         System.out.println(x);
 
     }
