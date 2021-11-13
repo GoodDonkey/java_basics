8e7315983078026e13025ade17f2201c116a39a4
GoodDonkey <president20500@gmail.com>
2021-11-14 00:49:24 +0900

    Array: another way of declaring array, length, sort

--- src/com/codewithme/Main.java
+++ src/com/codewithme/Main.java
@@ -7,9 +7,11 @@ public class Main {
 
     public static void main(String[] args) {
         // array: int array인 number를 길이 5로 정의
-        int[] numbers = new int[5];
-        numbers[0] = 1;
-        numbers[1] = 2;
+        int[] numbers = { 2, 3, 5, 1, 4};
+        System.out.println(numbers.length); // fixed length
+        System.out.println(Arrays.toString(numbers)); // int[]는 int로 initialize
+
+        Arrays.sort(numbers); // 정렬하기
         System.out.println(Arrays.toString(numbers)); // int[]는 int로 initialize
 
     }
