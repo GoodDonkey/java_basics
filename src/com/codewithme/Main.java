fd6dba8d56e55164b6fafaaf4613161bfe87ce39
GoodDonkey <president20500@gmail.com>
2021-11-13 23:14:42 +0900

    Primitive Types vs Reference Types: String type and the members

--- src/com/codewithme/Main.java
+++ src/com/codewithme/Main.java
@@ -1,13 +1,25 @@
 package com.codewithme;
 
-import java.awt.*;
+import java.util.Locale;
 
 public class Main {
 
     public static void main(String[] args) {
-        Point point1 = new Point(1, 1);
-        Point point2 = point1;
-        point1.x = 2;
-        System.out.println(point2);
+        String message = "Hello World" + "!!";
+
+        // class이기 때문에 String의 멤버 메소드 사용 가능.
+        System.out.println(message.endsWith("!!")); // "!!"로 끝나는지 true, false를 반환.
+        System.out.println(message.length());
+        System.out.println(message.indexOf("e")); // 위치 반환
+        System.out.println(message.indexOf("sky")); // 없으면 -1을 반환.
+        System.out.println(message.replace("!", "*")); // !를 *로 바꾼다.
+        System.out.println(message.toLowerCase(Locale.ROOT)); //
+        System.out.println(message.toUpperCase(Locale.ROOT)); //
+
+        message = "    Hello World" + "!!    ";
+        System.out.println(message);
+        System.out.println(message.trim()); // 양쪽 whitespace를 자름.
+
+
     }
 }
