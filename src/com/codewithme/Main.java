81c4070679956665385be7eac2c96b9f7664996e
hungrydonkey <president20500@gmail.com>
2021-11-14 09:13:02 +0900

    Reading Input: String:  next, nextLine, trim

--- src/com/codewithme/Main.java
+++ src/com/codewithme/Main.java
@@ -7,8 +7,16 @@ public class Main {
     public static void main(String[] args) {
         // Reading Input
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Age: ");
-        byte age = scanner.nextByte();
-        System.out.println("You are " + age);
+        System.out.print("Name: ");
+
+//        String name = scanner.next(); // 하나의 토큰만 받는다.
+//        System.out.println("You are " + name);
+
+//        String name2 = scanner.nextLine(); // 한 줄 전체를 받는다 (공백포함)
+//        System.out.println("You are " + name2);
+
+        String name3 = scanner.nextLine().trim(); // 한 줄 전체를 받는다 (좌우 공백제외)
+        System.out.println("You are " + name3);
+
     }
 }
