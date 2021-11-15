19598651bb761533b37e035eba1bc8859610a32c
hungrydonkey <president20500@gmail.com>
2021-11-15 19:25:48 +0900

    While Loops: for to while

--- src/com/codewithme/Main.java
+++ src/com/codewithme/Main.java
@@ -5,10 +5,14 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++)
-            System.out.println("Hello World" + i);
-
         for (int i = 5; i > 0; i--)
             System.out.println("Hello World" + i);
+
+        // 위 for 문을 while문으로 표현: 이 경우, for문이 더 깔끔.
+        int i = 5;
+        while (i > 0) {
+            System.out.println("Hello World" + i);
+            i--;
+        }
     }
 }
