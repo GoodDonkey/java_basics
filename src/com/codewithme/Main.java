b3fee905b94effa291be1893704248f1060be24f
hungrydonkey <president20500@gmail.com>
2021-11-15 19:25:48 +0900

    While Loops: use while loop when you don't know how many you execute something

--- src/com/codewithme/Main.java
+++ src/com/codewithme/Main.java
@@ -1,18 +1,18 @@
 package com.codewithme;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 5; i > 0; i--)
-            System.out.println("Hello World" + i);
-
-        // 위 for 문을 while문으로 표현: 이 경우, for문이 더 깔끔.
-        int i = 5;
-        while (i > 0) {
-            System.out.println("Hello World" + i);
-            i--;
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        // 받은 숫자를 그대로 프린트하기. quit을 받으면 while문이 끝남.
+        while (!input.equals("quit")) {
+            System.out.print("Input: ");
+            input = scanner.next().toLowerCase(Locale.ROOT);
+            System.out.println(input);
         }
     }
 }
