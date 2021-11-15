c8ed40f046a63dae5e96d72acaefa5bc53ddc4b2
hungrydonkey <president20500@gmail.com>
2021-11-15 16:55:01 +0900

    if statements: with less {}'s

--- src/com/codewithme/Main.java
+++ src/com/codewithme/Main.java
@@ -7,10 +7,10 @@ public class Main {
         if (temp > 30) {
             System.out.println("It's hot day");
             System.out.println("Drink water");
-        } else if (temp > 20) {
-            System.out.println("Beautiful day");
-        } else {
-            System.out.println("Cold day");
         }
+        else if (temp > 20)
+            System.out.println("Beautiful day");
+        else
+            System.out.println("Cold day");
     }
 }
