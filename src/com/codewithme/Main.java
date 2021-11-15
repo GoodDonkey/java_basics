83fc90bd4bbc8077a058675a2fb51aefcb274823
hungrydonkey <president20500@gmail.com>
2021-11-15 19:25:48 +0900

    While Loops: there should be how to 'break' when using while (true)

--- src/com/codewithme/Main.java
+++ src/com/codewithme/Main.java
@@ -9,7 +9,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String input = "";
         // 받은 숫자를 그대로 프린트하기. quit을 받으면 while문이 끝남.
-        while (!input.equals("quit")) {
+        while (true) {
             System.out.print("Input: ");
             input = scanner.next().toLowerCase(Locale.ROOT);
             if (input.equals("pass")) // pass를 입력받으면 다음 loop로 진행한다.
