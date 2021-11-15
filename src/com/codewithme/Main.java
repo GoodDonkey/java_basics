4714e76648880fa105a6e844efed2e6b96ac77bb
hungrydonkey <president20500@gmail.com>
2021-11-15 19:21:16 +0900

    For Loops: incrementing, decrementing

--- src/com/codewithme/Main.java
+++ src/com/codewithme/Main.java
@@ -5,32 +5,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // 5로 나누어 떨어지면 Fizz
-        // 3으로 나누어 떨어지면 Buzz
-        // 5 또는 3으로 나누어 떨어지면 FizzBuzz
-        // 모두 아니면 숫자 그대로
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Number: ");
+        for (int i = 0; i < 5; i++)
+            System.out.println("Hello World" + i);
 
-        // input
-        String numberInput = scanner.next().trim();
-
-        // initialization
-        int number = Integer.parseInt(numberInput);
-        String printing = numberInput;
-        boolean isDiv5 = false;
-
-        if (number % 5 == 0) {
-            printing = "Fizz";
-            isDiv5 = true;
-        }
-        if (number % 3 == 0) {
-            if (isDiv5) printing = printing.concat("Buzz");
-            else
-                printing = "Buzz";
-        }
-
-        System.out.println(printing);
-
+        for (int i = 5; i > 0; i--)
+            System.out.println("Hello World" + i);
     }
 }
