6c7d625a3983fe6cdf994575e171bed80edb4d7f
hungrydonkey <president20500@gmail.com>
2021-11-15 16:55:01 +0900

    if statements: shortening simple if statements

--- src/com/codewithme/Main.java
+++ src/com/codewithme/Main.java
@@ -3,14 +3,41 @@ package com.codewithme;
 public class Main {
 
     public static void main(String[] args) {
-        int temp = 32;
-        if (temp > 30) {
-            System.out.println("It's hot day");
-            System.out.println("Drink water");
+
+        /* 오류
+        int income = 120_000;
+        if (income > 100_000) {
+            boolean hasHighIncome = true; // if 안에서만 동작
         }
-        else if (temp > 20)
-            System.out.println("Beautiful day");
+        System.out.println(hasHighIncome);
+        */
+
+//        -----------
+
+       /*  1. 미리 선언하면 if 밖에서도 사용할 수 있다.
+        int income = 120_000;
+        boolean hasHighIncome;
+        if (income > 100_000)
+            hasHighIncome = true;
         else
-            System.out.println("Cold day");
+            hasHighIncome = false;
+        System.out.println(hasHighIncome);
+        */
+
+//        ---------
+
+        /* 2. 변수 기본값을 false로 해두면 else문을 쓰지 않아도 된다.
+        int income = 120_000;
+        boolean hasHighIncome = false;
+        if (income > 100_000)
+            hasHighIncome = true;
+        System.out.println(hasHighIncome);
+        */
+
+        // 결론: if 문을 쓰자 않아도 된다.
+        int income = 120_000;
+        boolean hasHighIncome = (income > 100_000); // logical statement는 () 로 감싸는 것이 더 읽기 좋다.
+        System.out.println(hasHighIncome);
+
     }
 }
