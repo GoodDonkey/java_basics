d8b3001d0a3b6df18691662962b0513f0cde48c0
hungrydonkey <president20500@gmail.com>
2021-11-18 16:12:23 +0900

    Classes and Objects: Memory Allocation

--- src/com/codewithme/Main.java
+++ src/com/codewithme/Main.java
@@ -3,12 +3,12 @@ package com.codewithme;
 public class Main {
 
     public static void main(String[] args) {
-        var textBox1 = new TextBox(); // var를 쓰면 부른 클래스의 타입을 찾아 정의함.
-        textBox1.setText("Box 1");
+        var textBox1 = new TextBox();
+        var textBox2 = textBox1; // TextBox 클래스라는 것을 파악함.
+        // 즉, 이 경우 하나의 heap 주소를 두개의 변수가 가리키고 있는 것.
+        // 그러므로 하나의 객체로 값을 바꾸면 다른 하나도 바뀐다.
+        textBox2.setText("Hello World");
         System.out.println(textBox1.text);
 
-        var textBox2 = new TextBox();
-        textBox2.setText("Box 2");
-        System.out.println(textBox2.text);
     }
 }
