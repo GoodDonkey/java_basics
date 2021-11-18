3a26d11e4c163d4c8d7892ea69862aa7325079dc
hungrydonkey <president20500@gmail.com>
2021-11-18 16:12:23 +0900

    Classes and Objects: getters and setters

--- src/com/codewithme/Employee.java
+++ src/com/codewithme/Employee.java
@@ -1,11 +1,32 @@
 package com.codewithme;
 
 public class Employee {
-    public int baseSalary; // 항상 같은 값이라고 가정.
-    public int hourlyRate; // 항상 같은 값이라고 가정.
+    private int baseSalary; // private 으로 정의하고 setter, getter를 이용해 값을 넣고, 조회한다.
+    private int hourlyRate; // 항상 같은 값이라고 가정.
 
     // extraHours 는 매달 달라진다고 가정.
     public int calculateWage(int extraHours) {
         return baseSalary + (hourlyRate * extraHours);
+    }
+
+    public void setBaseSalary(int baseSalary) {
+        if (baseSalary <= 0)
+            throw new IllegalArgumentException("Salary cannot be 0 or less."); // java library에 있는 exception을 사용함.
+        this.baseSalary = baseSalary;
+
+    }
+
+    public int getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+        if (hourlyRate <= 0)
+            throw new IllegalArgumentException("Hourly Rate cannot be 0 or less.");
+        this.hourlyRate = hourlyRate;
+    }
+
+    public int getHourlyRate() {
+        return hourlyRate;
     }
 }
--- src/com/codewithme/Main.java
+++ src/com/codewithme/Main.java
@@ -4,8 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
         var employee = new Employee();
-        employee.baseSalary = 50_000;
-        employee.hourlyRate = 20;
+
+        // set
+        employee.setBaseSalary(50_000);
+        employee.setHourlyRate(20);
+
+        // get
+        System.out.println(employee.getBaseSalary());
+        System.out.println(employee.getHourlyRate());
+
+        // 계산
         int wage = employee.calculateWage(10);
         System.out.println(wage);
     }
