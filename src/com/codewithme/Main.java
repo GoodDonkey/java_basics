d25220d2dbcbd86747cdcdb4c9c326f6ccd095ab
hungrydonkey <president20500@gmail.com>
2021-12-02 20:38:55 +0900

    Interfaces: Dependency Injection: Method Injection

--- src/com/codewithme/Main.java
+++ src/com/codewithme/Main.java
@@ -4,12 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         var calculator = new TaxCalculator2018(100_000);
-        var report = new TaxReport(calculator);
-        report.show();
-
-        // report에 다른 calculator를 setter를 사용해 주입한다.
-        report.setCalculator(new TaxCalculator2019());
-        report.show();
-
+        var report = new TaxReport();
+        report.show(calculator);
+        // 메서드에 직접 클래스를 주입한다.
+        report.show(new TaxCalculator2019());
     }
 }
