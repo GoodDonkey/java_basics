890e7e85bcd8f6f013d3ddb0c009f1e3b653d8f5
hungrydonkey <president20500@gmail.com>
2021-12-31 15:37:22 +0900

    Exceptions: try-with-resources statement for managing resources
    order of multiple catch phrases may matter.

--- Pt_3/src/com/codewithme/exceptions/ExceptionsDemo.java
+++ Pt_3/src/com/codewithme/exceptions/ExceptionsDemo.java
@@ -1,25 +1,18 @@
 package com.codewithme.exceptions;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class ExceptionsDemo {
     public static void show() {
-        FileReader reader = null;
-        try {
-            reader = new FileReader("file.txt");
-            var value = reader.read(); // 읽을 수 없으면 IOException
-            reader.close(); // close로 자원을 반납해야
+        try (
+                var reader = new FileReader("file.txt");
+                var writer = new FileWriter("...");
+        ) {
+            var value = reader.read();
         } catch (IOException e) {
             System.out.println("Could not read data");
-        } finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
 }
