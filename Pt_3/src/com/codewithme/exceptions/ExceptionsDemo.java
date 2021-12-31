f5ae582884104525e864a96ba94dd8ddca7084ac
hungrydonkey <president20500@gmail.com>
2021-12-31 15:37:22 +0900

    Exceptions: catching multiple types of exceptions
    order of multiple catch phrases may matter.

--- Pt_3/src/com/codewithme/exceptions/ExceptionsDemo.java
+++ Pt_3/src/com/codewithme/exceptions/ExceptionsDemo.java
@@ -2,14 +2,23 @@ package com.codewithme.exceptions;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     public static void show() {
         try {
             var reader = new FileReader("file.txt");
-            System.out.println("File opened");
-        } catch (FileNotFoundException ex) {
-            System.out.println(ex.getMessage()); // FileNotFoundException에 정의된 에러메시지를 직접 출력한다.
+            var value = reader.read(); // 읽을 수 없으면 IOException
+            new SimpleDateFormat().parse("");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace(); // 예외를 유발한 메서드의 trace를 포함하고 있다.
+            System.out.println("File does not exist");
+        } catch (IOException e) {
+            System.out.println("Could not read data");
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
     }
 }
