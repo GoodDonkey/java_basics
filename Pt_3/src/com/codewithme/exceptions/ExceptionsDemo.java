39fdbb894ad95c93a7a9a3692c11b1ed3d1e3bdd
hungrydonkey <president20500@gmail.com>
2021-12-31 15:37:22 +0900

    Exceptions: releasing resources with finally block
    order of multiple catch phrases may matter.

--- Pt_3/src/com/codewithme/exceptions/ExceptionsDemo.java
+++ Pt_3/src/com/codewithme/exceptions/ExceptionsDemo.java
@@ -1,24 +1,25 @@
 package com.codewithme.exceptions;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     public static void show() {
+        FileReader reader = null;
         try {
-            var reader = new FileReader("file.txt");
+            reader = new FileReader("file.txt");
             var value = reader.read(); // 읽을 수 없으면 IOException
-            new SimpleDateFormat().parse("");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace(); // 예외를 유발한 메서드의 trace를 포함하고 있다.
-            System.out.println("File does not exist");
+            reader.close(); // close로 자원을 반납해야
         } catch (IOException e) {
             System.out.println("Could not read data");
-        } catch (ParseException e) {
-            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
