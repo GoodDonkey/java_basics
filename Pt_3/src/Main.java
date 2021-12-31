542b8ac1d570e9a48b50e60d9406117a0a353bdd
hungrydonkey <president20500@gmail.com>
2021-12-31 15:37:22 +0900

    Generics: implementing Comparable Interface in Generic Class
    Exceptions may be thrown because of illegal casting

--- Pt_3/src/Main.java
+++ Pt_3/src/Main.java
@@ -1,9 +1,16 @@
-import com.codewithme.generics.GenericList;
-import com.codewithme.generics.List;
 import com.codewithme.generics.User;
 
 public class Main {
 
     public static void main(String[] args) {
+        var user1 = new User(10);
+        var user2 = new User(20);
+
+        if (user1.compareTo(user2) < 0)
+            System.out.println("user1 < user2");
+        else if (user1.compareTo(user2) == 0)
+            System.out.println("user1 == user2");
+        else
+            System.out.println("user1 > user2");
     }
 }
--- Pt_3/src/com/codewithme/generics/User.java
+++ Pt_3/src/com/codewithme/generics/User.java
@@ -1,4 +1,18 @@
 package com.codewithme.generics;
 
-public class User {
+public class User implements Comparable<User>{
+    private int points;
+
+    public User(int points) {
+        this.points = points;
+    }
+
+    @Override
+    public int compareTo(User other) {
+        // User를 point로 비교한다고 가정해보자.
+        // this < o     -> -1
+        // this == o    -> 0
+        // this > o     -> 1
+        return points - other.points; // 이와 같이 정의하고 동작은 call할 때 정의한다.
+    }
 }
