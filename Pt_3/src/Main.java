227dfd82f543819ee1929d10e3438827bec2f39f
hungrydonkey <president20500@gmail.com>
2021-12-31 15:37:22 +0900

    Generics: boxing and unboxing in generic list
    Exceptions may be thrown because of illegal casting

--- Pt_3/src/Main.java
+++ Pt_3/src/Main.java
@@ -5,8 +5,14 @@ import com.codewithme.generics.User;
 public class Main {
 
     public static void main(String[] args) {
-        GenericList<User> list = new GenericList<>();
-        list.add(new User());
-        User user = list.get(0);
+
+//        new GenericList<float>(); // Type argument cannot be of primitive type
+        // int -> Integer
+        // float -> Float
+        // boolean -> Boolean 와 같이 reference type으로 정의해야 가능하다.
+
+        GenericList<Integer> numbers = new GenericList<>();
+        numbers.add(1); // Boxing: 컴파일러가 primitive type 값을 box에 담아 처리한다.
+        int number = numbers.get(0); // Unboxing: 컴파일러가 box에 있는 int에서 꺼내온다.
     }
 }
