1e0fa48c6c29e0286cbe79639bf6453f85302287
hungrydonkey <president20500@gmail.com>
2021-12-31 15:37:22 +0900

    Generics: problem of using Object type class
    Exceptions may be thrown because of illegal casting

--- Pt_3/src/Main.java
+++ Pt_3/src/Main.java
@@ -1,9 +1,14 @@
 import com.codewithme.generics.List;
+import com.codewithme.generics.User;
 
 public class Main {
 
     public static void main(String[] args) {
         var list = new List();
-        list.add(1);
+        list.add(1); // 컴파일러가 reference type으로 casting한다.
+        list.add("1"); // Object를 받으므로 String도 가능
+        list.add(new User()); // Object를 받으므로 User도 가능
+
+        int number = (int)list.get(1);
     }
 }
--- Pt_3/src/com/codewithme/generics/List.java
+++ Pt_3/src/com/codewithme/generics/List.java
@@ -1,14 +1,14 @@
 package com.codewithme.generics;
 
 public class List {
-    private int[] items = new int[10];
+    private Object[] items = new Object[10];
     private int  count;
 
-    public void add(int item) {
+    public void add(Object item) {
         items[count++] = item; // add 를 call 할 때마다 증가된 index에 저장.
     }
 
-    public int get(int index) {
+    public Object get(int index) {
         return items[index];
     }
 }
