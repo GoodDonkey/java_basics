279f257b3bc14bbbe5713f0d516a0c1ae992b647
hungrydonkey <president20500@gmail.com>
2022-01-01 19:12:34 +0900

    Lambda: combining Predicates: and, or, negate

--- Pt_3/src/com/codewithme/lambda/LambdasDemo.java
+++ Pt_3/src/com/codewithme/lambda/LambdasDemo.java
@@ -1,13 +1,23 @@
 package com.codewithme.lambda;
 
-import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class LambdasDemo {
 
     public static void show() {
-        Predicate<String> isLongerThan5 = str -> str.length() > 5;
-        var result = isLongerThan5.test("sky");
-        System.out.println(result);
+        var testString = "key:value";
+
+        Predicate<String> hasLeftBrace = str -> str.startsWith("{");
+        Predicate<String> hasRightBrace = str -> str.endsWith("}");
+
+        Predicate<String> hasLeftAndRightBraces = hasLeftBrace.and(hasRightBrace);
+        System.out.println(hasLeftAndRightBraces.test(testString));
+
+        Predicate<String> or = hasLeftBrace.or(hasRightBrace);
+        System.out.println(or.test(testString));
+
+        Predicate<String> negate = hasLeftBrace.negate();
+        System.out.println(negate.test(testString));
+
     }
 }
