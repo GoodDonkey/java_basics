0e7236a232a6a727233f8d3b5cdda691db48a293
hungrydonkey <president20500@gmail.com>
2022-01-01 19:12:34 +0900

    Lambda: Predicate Interface

--- Pt_3/src/com/codewithme/lambda/LambdasDemo.java
+++ Pt_3/src/com/codewithme/lambda/LambdasDemo.java
@@ -1,40 +1,13 @@
 package com.codewithme.lambda;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class LambdasDemo {
 
     public static void show() {
-        // "key:value"
-        // first: "key=value"
-        // second: "{key=value}"
-        // third: "({key=value})"
-        Function<String, String> replacecolon = str -> str.replace(":", "=");
-        Function<String, String> addCurlyBraces = str -> "{" + str + "}";
-        Function<String, String> addRoundBraces = str -> "(" + str + ")";
-
-        // Declarative Programming
-        var result = replacecolon.andThen(addCurlyBraces)
-                                 .andThen(addRoundBraces)
-                                 .apply("key:value");
-        var result2 = replacecolon.andThen(addCurlyBraces)
-                                  .andThen(addCurlyBraces)
-                                  .andThen(addCurlyBraces)
-                                  .andThen(addCurlyBraces)
-                                  .apply("key:value");
-
-        // 순서 위와 같다.
-        var result3 = addRoundBraces.compose(addCurlyBraces)
-                                    .compose(replacecolon)
-                                    .apply("key:value");
-
-        // 순서 틀림
-        var result4 = addCurlyBraces.compose(addRoundBraces)
-                                    .compose(replacecolon)
-                                    .apply("key:value");
-
-        System.out.println(result2);
-        System.out.println(result3);
-        System.out.println(result4);
+        Predicate<String> isLongerThan5 = str -> str.length() > 5;
+        var result = isLongerThan5.test("sky");
+        System.out.println(result);
     }
 }
