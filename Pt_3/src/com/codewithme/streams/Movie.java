816e9df51c1c031fe1e2ec52922f113cf40da046
hungrydonkey <president20500@gmail.com>
2022-01-01 19:12:34 +0900

    Stream: Grouping elements

--- Pt_3/src/com/codewithme/streams/Movie.java
+++ Pt_3/src/com/codewithme/streams/Movie.java
@@ -3,10 +3,12 @@ package com.codewithme.streams;
 public class Movie {
     private String title;
     private int likes;
+    private Genre genre;
 
-    public Movie(String title, int likes) {
+    public Movie(String title, int likes, Genre genre) {
         this.title = title;
         this.likes = likes;
+        this.genre = genre;
     }
 
     public int getLikes() {
@@ -15,6 +17,10 @@ public class Movie {
 
     public String getTitle() {
         return title;
+    }
+
+    public Genre getGenre() {
+        return genre;
     }
 
     // sorted() 에서 사용하기 위해 정의할 수도 있지만 호출 환경에서 바로 lambda로 정의하면 상관없다.
--- Pt_3/src/com/codewithme/streams/StreamsDemo.java
+++ Pt_3/src/com/codewithme/streams/StreamsDemo.java
@@ -1,71 +1,42 @@
 package com.codewithme.streams;
 
 import java.util.*;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class StreamsDemo {
     public static void show() {
         var movies = List.of(
-                new Movie("b", 10),
-                new Movie("a", 20),
-                new Movie("c", 30),
-                new Movie("d", 20),
-                new Movie("e", 40)
+                new Movie("b", 10, Genre.THRILLER),
+                new Movie("a", 20, Genre.THRILLER),
+                new Movie("c", 30, Genre.ACTION),
+                new Movie("d", 20, Genre.ACTION),
+                new Movie("e", 40, Genre.ACTION)
         );
 
-        // Collectors.toList(), toSet()... 을 사용할 수 있다.
-        List<Movie> result = movies.stream()
-                                   .filter(m -> m.getLikes() > 10)
-                                   .collect(Collectors.toList());
+        // 기본적으로 value는 List 로 반환된다.
+        Map<Genre, List<Movie>> result = movies.stream()
+                                               .collect(Collectors.groupingBy(Movie::getGenre));
+        System.out.println(result);
 
-        Set<Movie> result2 = movies.stream()
-                                   .filter(m -> m.getLikes() > 10)
-                                   .collect(Collectors.toSet());
+        // value를 Set으로 반환되도록 만들 수 있다.
+        Map<Genre, Set<Movie>> result2 = movies.stream()
+                                               .collect(Collectors.groupingBy(Movie::getGenre,
+                                                                              Collectors.toSet()));
+        System.out.println(result2);
 
-        // title: likes 인 Map
-        // toMap(keyMapper, valueMapper)
-        Map<String, Integer> result3 = movies.stream()
-                                             .filter(m -> m.getLikes() > 10)
-                                             .collect(Collectors.toMap(Movie::getTitle, Movie::getLikes));
+        // 그루핑 하고 value 는 개수로
+        Map<Genre, Long> result3 = movies.stream()
+                                         .collect(Collectors.groupingBy(Movie::getGenre,
+                                                                        Collectors.counting()));
+        System.out.println(result3);
 
-        // value 로 Movie 객체를 담고 싶다면
-        Map<String, Movie> result4 = movies.stream()
-                                           .filter(m -> m.getLikes() > 10)
-                                           .collect(Collectors.toMap(Movie::getTitle, m -> m));
-
-        // Function.identity(): 객체를 받아서 그대로 반환하는 메서드. 위와 같은 표현.
-        Map<String, Movie> result5 = movies.stream()
-                                           .filter(m -> m.getLikes() > 10)
-                                           .collect(Collectors.toMap(Movie::getTitle, Function.identity()));
-
-        // Collectors.summingInt(ToIntFunction): mapper 함수를 이용해 int를 반환하는 Collector 객체 반환
-        // summingDouble, summingLong 등도 있다.
-        Integer result6 = movies.stream()
-                                .filter(m -> m.getLikes() > 10)
-                                .collect(Collectors.summingInt(Movie::getLikes));
-
-        // Collectors.summarizingInt(ToIntFunction): mapper 함수를 이용해 요약통계치를 가진 객체를 반환하는 Collector객체 반환.
-        // summarizingDouble, summarizingLong 도 있다.
-        IntSummaryStatistics result7 = movies.stream()
-                                             .filter(m -> m.getLikes() > 10)
-                                             .collect(Collectors.summarizingInt(Movie::getLikes));
-
-        System.out.println(result); // Movie 객체가 담겨있다
-        System.out.println(result2); // Movie 객체가 담겨있다
-        System.out.println(result3); // key value 로 담겨있다
+        // 그루핑하고 value는 title을 ", " 로 합치기
+        Map<Genre, String> result4 = movies.stream()
+                                           .collect(Collectors.groupingBy(Movie::getGenre,
+                                                                          Collectors.mapping(Movie::getTitle,
+                                                                                             Collectors.joining(
+                                                                                                     ", "))));
         System.out.println(result4);
-        System.out.println(result5);
-        System.out.println(result6);
-        System.out.println(result7);
-
-        // Collectors.joining(): String 을 합쳐서 반환하는 Collector 객체 반환. delimiter 줄 수 있다.
-        String result8 = movies.stream()
-                               .filter(m -> m.getLikes() > 10)
-                               .map(Movie::getTitle)
-                               .collect(Collectors.joining(","));
-        System.out.println(result8);
-
 
     }
 }
