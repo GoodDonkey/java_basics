e8f78d951e734fa7a44ec05198ae4107ac9b8af5
hungrydonkey <president20500@gmail.com>
2022-01-01 19:12:34 +0900

    Lambda: Function Interface

--- Pt_3/src/com/codewithme/lambda/LambdasDemo.java
+++ Pt_3/src/com/codewithme/lambda/LambdasDemo.java
@@ -1,23 +1,12 @@
 package com.codewithme.lambda;
 
-import java.util.function.IntSupplier;
-import java.util.function.Supplier;
+import java.util.function.Function;
 
 public class LambdasDemo {
 
     public static void show() {
-        Supplier<Double> getRandom = () -> Math.random(); // 이 메서드는 call 하기 전까지 실행되지 않는다.
-        var random = getRandom.get(); // Double
-        var random2 = Math.random(); // double
-        System.out.println(random);
-        System.out.println(Math.random());
-
-        // IntSupplier 사용해보기
-        String string = "Hello";
-        Integer length = string.length(); // Integer로 정의하였다.
-        System.out.println(length.getClass());
-
-        IntSupplier lengthInt = () -> length;
-        System.out.println(lengthInt.getAsInt()); // int 값이다. getClass()를 사용할 수 없다.
+        Function<String, Integer> map = str -> str.length();
+        var length = map.apply("Sky");
+        System.out.println(length);
     }
 }
