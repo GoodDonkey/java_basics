d3696fdb3db09bc1a540ebd7f115fff035d07b70
hungrydonkey <president20500@gmail.com>
2022-01-01 19:12:34 +0900

    Lambda: Supplier Interface

--- Pt_3/src/com/codewithme/lambda/LambdasDemo.java
+++ Pt_3/src/com/codewithme/lambda/LambdasDemo.java
@@ -1,16 +1,23 @@
 package com.codewithme.lambda;
 
-import java.util.List;
-import java.util.Locale;
-import java.util.function.Consumer;
+import java.util.function.IntSupplier;
+import java.util.function.Supplier;
 
 public class LambdasDemo {
 
     public static void show() {
-        List<String> list = List.of("a", "b", "c");
-        Consumer<String> print = item -> System.out.println(item);
-        Consumer<String> printUpperCase = item -> System.out.println(item.toUpperCase(Locale.ROOT));
+        Supplier<Double> getRandom = () -> Math.random(); // 이 메서드는 call 하기 전까지 실행되지 않는다.
+        var random = getRandom.get(); // Double
+        var random2 = Math.random(); // double
+        System.out.println(random);
+        System.out.println(Math.random());
 
-        list.forEach(print.andThen(printUpperCase).andThen(print)); // print 함수 실행 후 printUpperCase 실행한다.
+        // IntSupplier 사용해보기
+        String string = "Hello";
+        Integer length = string.length(); // Integer로 정의하였다.
+        System.out.println(length.getClass());
+
+        IntSupplier lengthInt = () -> length;
+        System.out.println(lengthInt.getAsInt()); // int 값이다. getClass()를 사용할 수 없다.
     }
 }
