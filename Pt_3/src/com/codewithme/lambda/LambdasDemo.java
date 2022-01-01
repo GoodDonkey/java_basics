a3244940dfdd3db55591a7e39f5eba8f508fdc99
hungrydonkey <president20500@gmail.com>
2022-01-01 19:12:34 +0900

    Lambda: lambda expression

--- Pt_3/src/com/codewithme/lambda/LambdasDemo.java
+++ Pt_3/src/com/codewithme/lambda/LambdasDemo.java
@@ -2,12 +2,11 @@ package com.codewithme.lambda;
 
 public class LambdasDemo {
     public static void show() {
-        greet(new Printer() {
-            @Override
-            public void print(String message) {
-                System.out.println(message);
-            }
-        });
+        // lambda expression 을 이용한 메서드 정의
+        greet(message -> System.out.println(message));
+
+        // 변수에 담을 수도 있다.
+        Printer printer = message -> System.out.println(message);
     }
 
     public static void greet(Printer printer) {
