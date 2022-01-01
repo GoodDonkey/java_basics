84fc0c3c0d1ce3d407ff524afd07ea076d4d25e5
hungrydonkey <president20500@gmail.com>
2022-01-01 19:12:34 +0900

    Stream: Mapping stream elements: flatMap()

--- Pt_3/src/com/codewithme/streams/StreamsDemo.java
+++ Pt_3/src/com/codewithme/streams/StreamsDemo.java
@@ -1,23 +1,12 @@
 package com.codewithme.streams;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class StreamsDemo {
     public static void show() {
-        var movies = List.of(
-            new Movie("a", 10),
-            new Movie("b", 20),
-            new Movie("c", 30)
-        );
-
-        // Movie의 제목만 얻고 싶다.
-        movies.stream() // Movie 객체를 가져온다.
-              .map(movie -> movie.getTitle()) // Movie 객체의 title 값을 가져온다.
-              .forEach(name -> System.out.println(name)); // title 을 프린트 한다.
-
-        // mapToInt: 반환타입이 int
-        movies.stream()
-              .mapToInt(movie -> movie.getLikes())
-              .forEach(likes -> System.out.println(likes));
+        var stream = Stream.of(List.of(1, 2, 3), List.of(4, 5, 6));
+        stream.flatMap(list -> list.stream()) // list의 stream객체를 반환하도록 정의하면, 개별 원소를 가지는 stream이 된다.
+              .forEach(n -> System.out.println(n));
     }
 }
