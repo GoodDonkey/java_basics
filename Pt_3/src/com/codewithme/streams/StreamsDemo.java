c55c876c5605a577ee1498bd741fac052e80dd3e
hungrydonkey <president20500@gmail.com>
2022-01-01 19:12:34 +0900

    Stream: Slicing Streams
    limit(), skip(), takeWhile(), dropWhile()

--- Pt_3/src/com/codewithme/streams/StreamsDemo.java
+++ Pt_3/src/com/codewithme/streams/StreamsDemo.java
@@ -9,13 +9,44 @@ public class StreamsDemo {
         var movies = List.of(
                 new Movie("a", 10),
                 new Movie("b", 20),
-                new Movie("c", 30)
+                new Movie("c", 30),
+                new Movie("d", 20),
+                new Movie("e", 40)
         );
 
-        Predicate<Movie> isPopular = m -> m.getLikes() > 10;
+        //limit()
+        System.out.println("-----drop-----");
         movies.stream()
-              .filter(isPopular)
-              .peek(m -> System.out.println(m.getLikes()))
+              .limit(2)
+              .forEach(m -> System.out.println(m.getTitle()));
+
+        // skip()
+        System.out.println("-----skip-----");
+        movies.stream()
+              .skip(2) // 2개를 생략한다.
+              .forEach(m -> System.out.println(m.getTitle()));
+
+//         - pagination
+//         1000 movies
+//         10 movies per page
+//         3rd page 를 보고싶다.
+//         skip(20) = skip( (page - 1) x pageSize)
+//         limit(10) = limit(pageSize)
+        movies.stream()
+              .skip(20)
+              .limit(10)
+              .forEach(m -> System.out.println(m.getTitle()));
+
+        // takeWhile: 30 미만인 것이 올 때까지만 값을 계속 넘긴다.
+        System.out.println("-----takeWhile-----");
+        movies.stream()
+              .takeWhile(m -> m.getLikes() < 30)
+              .forEach(m -> System.out.println(m.getTitle()));
+
+        // dropWhile
+        System.out.println("-----dropWhile-----");
+        movies.stream()
+              .dropWhile(m -> m.getLikes() < 30)
               .forEach(m -> System.out.println(m.getTitle()));
     }
 }
