7047ab181c6ddd33e2bbfa749ef7a2e008fa474a
hungrydonkey <president20500@gmail.com>
2022-01-01 19:12:34 +0900

    Collections: Iterable Interface

--- Pt_3/src/Main.java
+++ Pt_3/src/Main.java
@@ -1,13 +1,22 @@
 import com.codewithme.generics.GenericList;
-import com.codewithme.generics.Instructor;
-import com.codewithme.generics.User;
-import com.codewithme.generics.Utils;
+
+import java.util.Iterator;
 
 public class Main {
 
     public static void main(String[] args) {
-        var instructors = new GenericList<Instructor>();
-        var users = new GenericList<User>();
-        Utils.printUsers(instructors);
+        var list = new GenericList<String>();
+        Iterator<String> iterator = list.iterator();
+
+        /* Iterator객체를 사용하기 떄문에 아래와 같은 메서드를 사용할 수 있다.
+        * 이는 for each 문과 로직이 같다. */
+        while (iterator.hasNext()) {
+            String current = iterator.next();
+            System.out.println(current);
+        }
+
+        for (var item : list)
+            System.out.println(item);
+
     }
 }
--- Pt_3/src/com/codewithme/generics/GenericList.java
+++ Pt_3/src/com/codewithme/generics/GenericList.java
@@ -1,15 +1,21 @@
 package com.codewithme.generics;
 
 
-public class GenericList<T> {
-    private T[] items = (T[]) new Object[10];
+import java.util.Iterator;
+
+public class GenericList<T> implements Iterable<T>{
+    public T[] items = (T[]) new Object[10];
     private int count;
 
     public void add(T item) {
         items[count++] = item;
     }
-
     public T get(int index) {
         return items[index];
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
     }
 }
