f13a42637fc865108b1d4c449b340f0bd5ce5e55
hungrydonkey <president20500@gmail.com>
2022-01-01 19:12:34 +0900

    Lambda: variable capture in lambda expression

--- Pt_3/src/Main.java
+++ Pt_3/src/Main.java
@@ -3,6 +3,8 @@ import com.codewithme.lambda.LambdasDemo;
 public class Main {
 
     public static void main(String[] args) {
-        LambdasDemo.show();
+        LambdasDemo lambdasDemo = new LambdasDemo();
+        lambdasDemo.show();
+
     }
 }
--- Pt_3/src/com/codewithme/lambda/LambdasDemo.java
+++ Pt_3/src/com/codewithme/lambda/LambdasDemo.java
@@ -1,15 +1,23 @@
 package com.codewithme.lambda;
 
 public class LambdasDemo {
-    public static void show() {
+    private String prefix = "-";
+
+    public void show() {
+        String prefix2 = "!";
+
         // lambda expression 을 이용한 메서드 정의
-        greet(message -> System.out.println(message));
+        greet(message -> System.out.println(prefix + message));
+        prefix = "*";
+        greet(message -> System.out.println(prefix + message));
 
         // 변수에 담을 수도 있다.
-        Printer printer = message -> System.out.println(message);
+        final Printer printer = message -> System.out.println(prefix2 + message);
+//        prefix2 = "@"; // lambda가 사용하는 지역 변수 prefix2 는 변경되어서는 안됨.
+        greet(printer);
     }
 
-    public static void greet(Printer printer) {
+    public void greet(Printer printer) {
         printer.print("Hello World");
     }
 }
