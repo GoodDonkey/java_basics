08695b8e99fa5a0a1fc1d6f83e930911f1ee0659
hungrydonkey <president20500@gmail.com>
2022-01-01 19:12:34 +0900

    Stream: primitive type stream

--- Pt_3/src/com/codewithme/streams/StreamsDemo.java
+++ Pt_3/src/com/codewithme/streams/StreamsDemo.java
@@ -1,24 +1,15 @@
 package com.codewithme.streams;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class StreamsDemo {
     public static void show() {
-        var movies = List.of(
-                new Movie("b", 10, Genre.THRILLER),
-                new Movie("a", 20, Genre.THRILLER),
-                new Movie("c", 30, Genre.ACTION),
-                new Movie("d", 20, Genre.ACTION),
-                new Movie("e", 40, Genre.ACTION)
-        );
+        IntStream.rangeClosed(1, 5)
+                 .forEach(System.out::println);
 
-        Map<Boolean, String> result = movies.stream()
-                                            .collect(Collectors.partitioningBy(m -> m.getLikes() > 20,
-                                                                               Collectors.mapping(Movie::getTitle,
-                                                                                                  Collectors.joining(
-                                                                                                          ", "))));
-        System.out.println(result);
+        System.out.println("------------");
 
+        IntStream.range(1, 5)
+                .forEach(System.out::println);
     }
 }
