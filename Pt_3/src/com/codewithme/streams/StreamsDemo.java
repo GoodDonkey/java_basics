523c73a303614e9299cbec6c05e2b50d88b0c4d4
hungrydonkey <president20500@gmail.com>
2022-01-01 19:12:34 +0900

    Stream: creating streams with generate(), iterate()

--- Pt_3/src/com/codewithme/streams/StreamsDemo.java
+++ Pt_3/src/com/codewithme/streams/StreamsDemo.java
@@ -1,24 +1,21 @@
 package com.codewithme.streams;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.stream.Stream;
 
 public class StreamsDemo {
     public static void show() {
-        // Collection 은 stream을 사용할 수 있다.
-        Collection<Integer> x = null;
-//        x.stream();
+        // arbitrary number of objects
+        Stream.of(1, 2, 3, 4);
 
-        // List 를 정의하면 stream을 사용할 수 있다.
-        var list = new ArrayList<>();
-//        list.stream();
+        // generate() 메서드
+        // Supplier 이용: call 할때마다 랜덤 숫자가 나옴.
+        var stream = Stream.generate(() -> Math.random());
+        stream.limit(3)
+              .forEach(n -> System.out.println(n));
 
-        // array 자체는 stream 메서드가 없다.
-        // 대신 Arrays 클래스를 이용한다.
-        int[] numbers = {1, 2, 3};
-//        numbers.stream();
-        Arrays.stream(numbers)
-                .forEach(n -> System.out.println(n));
+        // iterate() 메서드
+        Stream.iterate(1, n -> n + 1)
+              .limit(10)
+              .forEach(n -> System.out.println(n));
     }
 }
