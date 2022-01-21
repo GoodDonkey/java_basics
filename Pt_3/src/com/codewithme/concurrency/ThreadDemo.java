0098b656a09fa5ea62d7548ee85197a3972c83ee
hungrydonkey <president20500@gmail.com>
2022-01-21 14:46:14 +0900

    Concurrency: interrupting a thread

--- Pt_3/src/com/codewithme/concurrency/ThreadDemo.java
+++ Pt_3/src/com/codewithme/concurrency/ThreadDemo.java
@@ -3,17 +3,17 @@ package com.codewithme.concurrency;
 public class ThreadDemo {
 
     public static void show() {
-        System.out.println(Thread.currentThread().getName()); // main Thread 이다.
-
         Thread thread = new Thread(new DownloadFileTask());
         thread.start();
 
         try {
-            thread.join();
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("File is ready to be scanned");
+
+        thread.interrupt(); // 직접 쓰레드를 멈출 수는 없다.
+
 
     }
 }
