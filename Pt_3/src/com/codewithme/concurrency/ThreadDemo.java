039ce2b8cfbea28eaac1adfb318229b9f4540adc
hungrydonkey <president20500@gmail.com>
2022-01-21 14:36:37 +0900

    Concurrency: joining a thread

--- Pt_3/src/com/codewithme/concurrency/ThreadDemo.java
+++ Pt_3/src/com/codewithme/concurrency/ThreadDemo.java
@@ -5,9 +5,15 @@ public class ThreadDemo {
     public static void show() {
         System.out.println(Thread.currentThread().getName()); // main Thread 이다.
 
-        for (var i = 0; i < 15; i++) {
-            Thread thread = new Thread(new DownloadFileTask());
-            thread.start();
+        Thread thread = new Thread(new DownloadFileTask());
+        thread.start();
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        System.out.println("File is ready to be scanned");
+
     }
 }
