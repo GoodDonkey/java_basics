5cc568c34c4d9f5153b1488285c90bd53ebb4b7a
hungrydonkey <president20500@gmail.com>
2022-01-21 14:29:56 +0900

    Concurrency: pausing a thread

--- Pt_3/src/com/codewithme/concurrency/ThreadDemo.java
+++ Pt_3/src/com/codewithme/concurrency/ThreadDemo.java
@@ -5,7 +5,7 @@ public class ThreadDemo {
     public static void show() {
         System.out.println(Thread.currentThread().getName()); // main Thread 이다.
 
-        for (var i = 0; i < 10; i++) {
+        for (var i = 0; i < 15; i++) {
             Thread thread = new Thread(new DownloadFileTask());
             thread.start();
         }
