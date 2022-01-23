9a9a0de8336330086c90e7909209cf4ab7124d04
hungrydonkey <president20500@gmail.com>
2022-01-23 16:57:23 +0900

    Concurrency: Synchronized Collections

--- Pt_3/src/com/codewithme/concurrency/ThreadDemo.java
+++ Pt_3/src/com/codewithme/concurrency/ThreadDemo.java
@@ -1,29 +1,32 @@
 package com.codewithme.concurrency;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
 public class ThreadDemo {
 
     public static void show() {
-        var status = new DownloadStatus();
+        Collection<Integer> collection = Collections.synchronizedCollection(new ArrayList<>());
 
-        var thread1 = new Thread(new DownloadFileTask(status));
+        var thread1 = new Thread(() -> {
+            collection.addAll(Arrays.asList(1, 2, 3));
+        });
         var thread2 = new Thread(() -> {
-
-            // isDone 이 true 가 될떄까지 기다린다.
-            while (!status.isDone()) {
-                synchronized (status) {
-                    try {
-                        status.wait(); // 다른 쓰레드가 깨울 때까지(notify) sleep(while문을 돌지 않음)
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-
-            // status 의 결과를 찍는다.
-            System.out.println(status.getTotalBytes());
+            collection.addAll(Arrays.asList(4, 5, 6));
         });
 
-        thread1.start(); // 끝나면 isDone 이 true가 된다.
-        thread2.start(); // status 가 true가 되면 끝나야하는데 끝나지 않는다.
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(collection);
     }
 }
