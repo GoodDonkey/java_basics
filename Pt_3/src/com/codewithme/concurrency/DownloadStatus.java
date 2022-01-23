eb2b8e3bd6af696c38c7d2edd6ce34e8d0f0332a
hungrydonkey <president20500@gmail.com>
2022-01-23 15:39:57 +0900

    Concurrency: using Atomic Object

--- Pt_3/src/com/codewithme/concurrency/DownloadStatus.java
+++ Pt_3/src/com/codewithme/concurrency/DownloadStatus.java
@@ -1,13 +1,35 @@
 package com.codewithme.concurrency;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class DownloadStatus {
-    private int totalBytes;
+    private volatile boolean isDone;
+    private AtomicInteger totalBytes = new AtomicInteger();
+    private int totalFiles;
+
 
     public int getTotalBytes() {
-        return totalBytes;
+        return totalBytes.get();
     }
 
     public void incrementTotalBytes() {
-        totalBytes++;
+        totalBytes.incrementAndGet(); // ++a 와 같지만 증가만 목적이므로 뭐든 괜찮.
+    }
+
+    public synchronized void incrementTotalFiles() {
+        totalFiles++;
+    }
+
+    public int getTotalFiles() {
+        return totalFiles;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void done() {
+        isDone = true;
+        System.out.println("task done:  -> " + this.hashCode());
     }
 }
