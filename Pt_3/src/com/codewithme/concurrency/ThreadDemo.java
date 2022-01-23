6efca2fbd065a8b7bf8201e1bb8ad2c58387743e
hungrydonkey <president20500@gmail.com>
2022-01-23 17:25:20 +0900

    Concurrency: Concurrent Collections

--- Pt_3/src/com/codewithme/concurrency/ThreadDemo.java
+++ Pt_3/src/com/codewithme/concurrency/ThreadDemo.java
@@ -1,32 +1,14 @@
 package com.codewithme.concurrency;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ThreadDemo {
 
     public static void show() {
-        Collection<Integer> collection = Collections.synchronizedCollection(new ArrayList<>());
-
-        var thread1 = new Thread(() -> {
-            collection.addAll(Arrays.asList(1, 2, 3));
-        });
-        var thread2 = new Thread(() -> {
-            collection.addAll(Arrays.asList(4, 5, 6));
-        });
-
-        thread1.start();
-        thread2.start();
-
-        try {
-            thread1.join();
-            thread2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println(collection);
+        Map<Integer, String> map = new ConcurrentHashMap<>();
+        map.put(1, "a");
+        map.get(1);
+        map.remove(1);
     }
 }
